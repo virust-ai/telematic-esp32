b6548000a17d22563eac000218cfbe50551fe726
Tu Nguyen <tu.nguyen@ionmobility.com>
2024-12-15 22:22:08 +0700

    Implement simple MQTT connection

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,39 @@
 #![no_main]
 
 use embassy_executor::Spawner;
+use embassy_net::{tcp::TcpSocket, Ipv4Address, Stack, StackResources};
+use embassy_time::{Duration, Timer};
 use esp_backtrace as _;
 use esp_hal::{
-    prelude::*,
-    timer::timg::TimerGroup,
-    twai::{self, TwaiMode},
+    prelude::*, rng::Rng, timer::timg::TimerGroup, twai::{self, TwaiMode}
 };
 
-use log::{error, info};
+use esp_println::println;
+use esp_wifi::{
+    init,
+    wifi::{ClientConfiguration, Configuration, WifiController, WifiDevice, WifiEvent, WifiStaDevice, WifiState}, EspWifiController,
+};
+
+// MQTT related imports
+use rust_mqtt::{
+    client::{client::MqttClient, client_config::ClientConfig},
+    packet::v5::reason_codes::ReasonCode,
+    utils::rng_generator::CountingRng,
+};
+
+use log::info;
+
+macro_rules! mk_static {
+    ($t:ty,$val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        #[deny(unused_attributes)]
+        let x = STATIC_CELL.uninit().write(($val));
+        x
+    }};
+}
+
+const SSID: &str = "An Son";
+const PASSWORD: &str = "1223334444";
 
 #[derive(Debug)]
 #[allow(dead_code)]
@@ -19,17 +44,40 @@ struct CanFrame {
 }
 
 #[esp_hal_embassy::main]
-async fn main(_spawner: Spawner) -> ! {
+async fn main(spawner: Spawner) -> ! {
     esp_println::logger::init_logger_from_env();
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::max();
         config
     });
-
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    esp_hal_embassy::init(timg0.timer0);
+    let mut rng = Rng::new(peripherals.RNG);
+    let init = &*mk_static!(
+        EspWifiController<'static>,
+        init(timg0.timer0, rng.clone(), peripherals.RADIO_CLK).unwrap()
+    );
+    let wifi = peripherals.WIFI;
+    let (wifi_interface, controller) =
+        esp_wifi::wifi::new_with_mode(&init, wifi, WifiStaDevice).unwrap();
+        let config = embassy_net::Config::dhcpv4(Default::default());
 
+        let seed = (rng.random() as u64) << 32 | rng.random() as u64;
+    
+        // Init network stack
+        let stack = &*mk_static!(
+            Stack<WifiDevice<'_, WifiStaDevice>>,
+            Stack::new(
+                wifi_interface,
+                config,
+                mk_static!(StackResources<3>, StackResources::<3>::new()),
+                seed
+            )
+        );
+    
+        spawner.spawn(connection(controller)).ok();
+        spawner.spawn(net_task(&stack)).ok();
+    
     info!("Welcome to esp-diag version: 0.1.2\r");
 
     let tx_pin = peripherals.GPIO1;
@@ -52,15 +100,141 @@ async fn main(_spawner: Spawner) -> ! {
         },
     );
     let mut can = twai_config.start();
+    let mut rx_buffer = [0; 4096];
+    let mut tx_buffer = [0; 4096];
 
+    //wait until wifi connected
     loop {
-        match can.receive_async().await {
-            Ok(frame) => {
-                info!("Received CAN Frame: {:?}", frame);
-            },
-            Err(e) => {
-                error!("Failed to receive CAN frame: {:?}", e);
+        if stack.is_link_up() {
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+
+    println!("Waiting to get IP address...");
+    loop {
+        if let Some(config) = stack.config_v4() {
+            println!("Got IP: {}", config.address); //dhcp IP address
+            break;
+        }
+        Timer::after(Duration::from_millis(500)).await;
+    }
+    loop {
+        Timer::after(Duration::from_millis(1_000)).await;
+
+        let mut socket = TcpSocket::new(&stack, &mut rx_buffer, &mut tx_buffer);
+
+        socket.set_timeout(Some(embassy_time::Duration::from_secs(10)));
+        let broker_ip = Ipv4Address::new(3, 125, 183, 56);
+        println!("Using hardcoded IP: {:?}", broker_ip);
+        // TODO: perform a DNS query
+        
+        let remote_endpoint = embassy_net::IpEndpoint {
+            addr: embassy_net::IpAddress::Ipv4(broker_ip),
+            port: 1883
+            
+        };
+        println!("connecting...");
+        let connection = socket.connect(remote_endpoint).await;
+        if let Err(e) = connection {
+            println!("connect error: {:?}", e);
+            continue;
+        }
+        println!("connected!");
+
+        let mut config = ClientConfig::new(
+            rust_mqtt::client::client_config::MqttVersion::MQTTv5,
+            CountingRng(20000),
+        );
+        config.add_max_subscribe_qos(rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1);
+        config.add_client_id("clientId-8rhWgBODCl");
+        config.max_packet_size = 100;
+        let mut recv_buffer = [0; 80];
+        let mut write_buffer = [0; 80];
+
+        let mut client =
+            MqttClient::<_, 5, _>::new(socket, &mut write_buffer, 80, &mut recv_buffer, 80, config);
+
+        match client.connect_to_broker().await {
+            Ok(()) => {}
+            Err(mqtt_error) => match mqtt_error {
+                ReasonCode::NetworkError => {
+                    println!("MQTT Network Error");
+                    continue;
+                }
+                _ => {
+                    println!("Other MQTT Error: {:?}", mqtt_error);
+                    continue;
+                }
             },
         }
+
+        loop {
+            if let Ok(_frame) = can.receive_async().await {
+                match client
+                    .send_message(
+                        "can/1",
+                        &[b'H', b'E', b'L', b'L', b'O', b'\n'],
+                        rust_mqtt::packet::v5::publish_packet::QualityOfService::QoS1,
+                        true,
+                    )
+                    .await
+                {
+                    Ok(()) => {}
+                    Err(mqtt_error) => match mqtt_error {
+                        ReasonCode::NetworkError => {
+                            println!("MQTT Network Error");
+                            continue;
+                        }
+                        _ => {
+                            println!("Other MQTT Error: {:?}", mqtt_error);
+                            continue;
+                        }
+                    },
+                }
+                Timer::after(Duration::from_millis(3000)).await;
+            }
+        }
     }
+}
+
+#[embassy_executor::task]
+async fn connection(mut controller: WifiController<'static>) {
+    println!("start connection task");
+    println!("Device capabilities: {:?}", controller.capabilities());
+    loop {
+        match esp_wifi::wifi::wifi_state() {
+            WifiState::StaConnected => {
+                // wait until we're no longer connected
+                controller.wait_for_event(WifiEvent::StaDisconnected).await;
+                Timer::after(Duration::from_millis(5000)).await
+            }
+            _ => {}
+        }
+        if !matches!(controller.is_started(), Ok(true)) {
+            let client_config = Configuration::Client(ClientConfiguration {
+                ssid: SSID.try_into().unwrap(),
+                password: PASSWORD.try_into().unwrap(),
+                ..Default::default()
+            });
+            controller.set_configuration(&client_config).unwrap();
+            println!("Starting wifi");
+            controller.start_async().await.unwrap();
+            println!("Wifi started!");
+        }
+        println!("About to connect...");
+
+        match controller.connect_async().await {
+            Ok(_) => println!("Wifi connected!"),
+            Err(e) => {
+                println!("Failed to connect to wifi: {e:?}");
+                Timer::after(Duration::from_millis(5000)).await
+            }
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn net_task(stack: &'static Stack<WifiDevice<'static, WifiStaDevice>>) {
+    stack.run().await
 }
