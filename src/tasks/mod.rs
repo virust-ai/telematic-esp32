15a89a0e8680eb9e563840223031b704676227bb
Tu Nguyen <tu.nguyen@ionmobility.com>
2025-02-07 15:29:37 +0700

    mqtt can work with certs from now

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -8,4 +8,7 @@ pub use mqtt::mqtt_handler;
 pub use quectel::{quectel_rx_handler, quectel_tx_handler};
 pub use wifi::{connection, net_task};
 
-const MQTT_CLIENT_ID: &str = "5680ff91-2d1c-4d0a-a8f7-f9c2a2066740";
+const MQTT_SERVERNAME: &str = "broker-s.ionmobility.net";
+const MQTT_CLIENT_ID: &str = "7f9ea02a-c93a-4b9d-b638-bf989865f9fe";
+const MQTT_USR_NAME: &str = "37c63374-e927-4a32-a2a2-ad8bfb7ee945";
+const MQTT_USR_PASS: [u8; 36] = *b"4b8ce895-83af-4f61-b469-f97ee2f18d4b";
